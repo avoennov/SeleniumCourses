3576a15daccc2e8bc90631f2cc85dcc8b3629cbe
avoennov <52177854+avoennov@users.noreply.github.com>
2019-11-21 22:26:59 +0300

    Less 2. Task 3. Create login scenario
    Added new module "litecart-tests"

---
+++ litecart-tests/src/test/java/ru/stqa/training/litecart/LoginTest.java
@@ -0,0 +1,38 @@
+package ru.stqa.training.litecart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.urlToBe;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start () {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testLogin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin@123$");
+        driver.findElement(By.name("login")).click();
+        wait.until(urlToBe("http://localhost/litecart/admin/"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}

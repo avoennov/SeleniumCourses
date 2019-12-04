cff94d3f6ae114afa2473bda1ac49eb02837a620
avoennov <52177854+avoennov@users.noreply.github.com>
2019-12-05 01:33:14 +0300

    Less 3. Task 4. FF+IE; Task 5. FF ESR; Task 6. FF Nightly

--- litecart-tests/src/test/java/ru/stqa/training/litecart/LoginTest.java
+++ litecart-tests/src/test/java/ru/stqa/training/litecart/LoginTest.java
@@ -7,28 +7,46 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import sun.plugin2.util.BrowserType;
-
+import java.util.concurrent.TimeUnit;
 import static org.openqa.selenium.support.ui.ExpectedConditions.urlToBe;
 
 public class LoginTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private FirefoxOptions options;
+    private final String browser = BrowserType.CHROME;  //Also possible set FIREFOX or IE for launch test in according browser
+    //private final String browser = "FirefoxESR";
+    //private final String browser = "FirefoxNightly";
 
     @Before
     public void start () {
-        driver = new ChromeDriver();
-        //driver = new FirefoxDriver();
-        //driver = new InternetExplorerDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            driver = new InternetExplorerDriver();
+        } else if (browser.equals("FirefoxESR")) {
+            options = new FirefoxOptions().setLegacy(true);
+            options.setBinary("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe");
+            driver = new FirefoxDriver(options);
+        } else if (browser.equals("FirefoxNightly")) {
+            options = new FirefoxOptions();
+            options.setBinary("C:\\Program Files\\Firefox Nightly\\firefox.exe");
+            driver = new FirefoxDriver(options);
+        }
         wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://localhost/litecart/admin/");
     }
 
     @Test
-    public void testLogin() {
-        driver.get("http://localhost/litecart/admin/");
+    public void testLogin(){
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin@123$");
         driver.findElement(By.name("login")).click();

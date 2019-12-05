0fe4ec519e5c45c024a8c6091c246182290f9904
avoennov <52177854+avoennov@users.noreply.github.com>
2019-12-06 01:02:22 +0300

    Less 4. Task 8. Check stickers on products

---
+++ litecart-tests/src/test/java/ru/stqa/training/litecart/tests/StickersTest.java
@@ -0,0 +1,44 @@
+package ru.stqa.training.litecart.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import ru.stqa.training.litecart.TestBase;
+
+public class StickersTest extends TestBase {
+
+    @Test
+    public void testStickers() throws InterruptedException {
+        driver.get("http://localhost/litecart/en/");
+        Thread.sleep(1000);
+
+        String[] sectionType = {"box-most-popular", "box-campaigns", "box-latest-products"};
+        String locator, itemName, stickerName, sectionName;
+        int itemsCount, stickerCount;
+        WebElement item;
+
+        for (String section : sectionType) {
+            sectionName = driver.findElement(By.xpath(".//div[@id='" + section + "']//h3[@class]")).getText();
+            System.out.println(sectionName);
+            locator = ".//div[@id='" + section + "']//li[@class]";
+            itemsCount = driver.findElements(By.xpath(locator)).size();
+            if (itemsCount > 0) {
+                for (int i = 1; i <= itemsCount; i++) {
+                    item = driver.findElement(By.xpath(locator + "[" + i + "]"));
+                    stickerCount = item.findElements(By.xpath(".//div[@class='image-wrapper']/div[@title]")).size();
+                    itemName = item.findElement(By.xpath(".//div[@class='name']")).getText();
+                    if (stickerCount == 1) {
+                        stickerName = item.findElement(By.xpath(".//div[@title]")).getText();
+                        System.out.println("[PASS] - Item {" + itemName + "} has sticker {" + stickerName + "}");
+                    }
+                    else if (stickerCount > 1)
+                        System.out.println("[FAIL] - Item {" + itemName + "} has stickers more than one");
+                    else
+                        System.out.println("[FAIL] - Item {" + itemName + "} doesn't have sticker");
+                }
+            }
+            else
+                System.out.println("[FAIL] - Items are absent");
+        }
+    }
+}
